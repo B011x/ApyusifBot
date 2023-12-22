d9d7640770d4ea3446f01e19fc3595befa6a8642
Zilzalll <146859836+Zilzalll@users.noreply.github.com>
2023-12-22 16:45:21 +0300

    Add files via upload

---
+++ ZelzalMusic/core/mongo.py
@@ -0,0 +1,18 @@
+#▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒✯ ʑᴇʟᴢᴀʟ_ᴍᴜsɪᴄ ✯▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+#▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒✯  T.me/ZThon   ✯▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+#▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒✯ T.me/Zelzal_Music ✯▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+
+from motor.motor_asyncio import AsyncIOMotorClient
+
+from config import MONGO_DB_URI
+
+from ..logging import LOGGER
+
+LOGGER("ميــوزك زدثــون").info("جـارِ الاتصـال بقاعـدة البيانـات . . .")
+try:
+    _mongo_async_ = AsyncIOMotorClient(MONGO_DB_URI)
+    mongodb = _mongo_async_.Anon
+    LOGGER("ميــوزك زدثــون").info("تم الاتصـال بقاعـدة البيانـات ...✓")
+except:
+    LOGGER(__name__).error("حدث خطأ اثناء الاتصال بقاعدة البيانات.")
+    exit()
