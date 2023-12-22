c197c61d58c8b85802b657a15fe465c357d88ad8
Zilzalll <146859836+Zilzalll@users.noreply.github.com>
2023-12-22 16:48:07 +0300

    Add files via upload

---
+++ ZelzalMusic/utils/logger.py
@@ -0,0 +1,41 @@
+#▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒✯ ʑᴇʟᴢᴀʟ_ᴍᴜsɪᴄ ✯▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+#▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒✯  T.me/ZThon   ✯▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+#▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒✯ T.me/Zelzal_Music ✯▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+
+from pyrogram.enums import ParseMode
+
+from ZelzalMusic import app
+from ZelzalMusic.utils.database import is_on_off
+from config import LOGGER_ID
+
+
+async def play_logs(message, streamtype):
+    if await is_on_off(2):
+        logger_text = f"""
+ٴ<b>•────‌‌‏✯ ʑ_ᴍᴜsɪᴄ ✯──‌‌‏─‌‌‏─•</b>
+<b>إشعـارات  الميـوزك 𝄞</b>
+ٴ<b>•────‌‌‏✯ ʑ_ᴍᴜsɪᴄ ✯──‌‌‏─‌‌‏─•</b>
+<b>- سيـدي المطـور 🧑🏻‍💻</b>
+<b>- هنـاك شخص يستخـدم الميـوزك حاليـاً 🎻</b>
+
+<b>- الاسم :</b> {message.from_user.mention}
+<b>- اليوزر :</b> @{message.from_user.username}
+<b>- ايدي المستخدم :</b> <code>{message.from_user.id}</code>
+
+<b>- اسم المجموعة :</b> {message.chat.title}
+<b>- يوزر المجموعة :</b> @{message.chat.username}
+<b>- ايدي المجموعة :</b> <code>{message.chat.id}</code>
+
+<b>- الطلب :</b> {message.text.split(None, 1)[1]}
+<b>- نوع التشغيل :</b> {streamtype}"""
+        if message.chat.id != LOGGER_ID:
+            try:
+                await app.send_message(
+                    chat_id=LOGGER_ID,
+                    text=logger_text,
+                    parse_mode=ParseMode.HTML,
+                    disable_web_page_preview=True,
+                )
+            except:
+                pass
+        return
---
+++ ZelzalMusic/utils/pastebin.py
@@ -0,0 +1,25 @@
+#▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒✯ ʑᴇʟᴢᴀʟ_ᴍᴜsɪᴄ ✯▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+#▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒✯  T.me/ZThon   ✯▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+#▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒✯ T.me/Zelzal_Music ✯▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+
+import aiohttp
+
+BASE = "https://batbin.me/"
+
+
+async def post(url: str, *args, **kwargs):
+    async with aiohttp.ClientSession() as session:
+        async with session.post(url, *args, **kwargs) as resp:
+            try:
+                data = await resp.json()
+            except Exception:
+                data = await resp.text()
+        return data
+
+
+async def ZelzalyBin(text):
+    resp = await post(f"{BASE}api/v2/paste", data=text)
+    if not resp["success"]:
+        return
+    link = BASE + resp["message"]
+    return link
