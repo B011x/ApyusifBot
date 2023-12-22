6d954830c9ff1f7158e31e348b120e8800360239
Zilzalll <146859836+Zilzalll@users.noreply.github.com>
2023-12-22 17:01:04 +0300

    Add files via upload

---
+++ ZelzalMusic/plugins/admins/stop.py
@@ -0,0 +1,28 @@
+#▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒✯ ʑᴇʟᴢᴀʟ_ᴍᴜsɪᴄ ✯▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+#▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒✯  T.me/ZThon   ✯▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+#▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒✯ T.me/Zelzal_Music ✯▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+
+from ZelzalMusic.plugins.play.filters import command
+from pyrogram import filters
+from pyrogram.types import Message
+
+from ZelzalMusic import app
+from ZelzalMusic.core.call import Zelzaly
+from ZelzalMusic.utils.database import set_loop
+from ZelzalMusic.utils.decorators import AdminRightsCheck
+from ZelzalMusic.utils.inline import close_markup
+from config import BANNED_USERS
+
+
+@app.on_message(
+    command(["/stop", "اسكت", "انهاء", "ايقاف"]) & filters.group & ~BANNED_USERS
+)
+@AdminRightsCheck
+async def stop_music(cli, message: Message, _, chat_id):
+    if not len(message.command) == 1:
+        return
+    await Zelzaly.stop_stream(chat_id)
+    await set_loop(chat_id, 0)
+    await message.reply_text(
+        _["admin_5"].format(message.from_user.mention), reply_markup=close_markup(_)
+    )
