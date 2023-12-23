926e0ba359866d7686e22591e49e8f90cdca8cc2
Zilzalll <146859836+Zilzalll@users.noreply.github.com>
2023-12-23 23:01:42 +0300

    Update channel.py

--- ZelzalMusic/plugins/play/channel.py
+++ ZelzalMusic/plugins/play/channel.py
@@ -19,7 +19,7 @@ async def playmode_(client, message: Message, _):
     if len(message.command) < 2:
         return await message.reply_text(_["cplay_1"].format(message.chat.title))
     query = message.text.split(None, 2)[1].lower().strip()
-    if (str(query)).lower() == "disable":
+    if (str(query)).lower() == "تعطيل":
         await set_cmode(message.chat.id, None)
         return await message.reply_text(_["cplay_7"])
     elif str(query) == "linked":
