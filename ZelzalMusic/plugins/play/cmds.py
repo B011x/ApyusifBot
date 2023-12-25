f49a8807abc1c1cca039995f05d831176c7301ca
Zilzalll <146859836+Zilzalll@users.noreply.github.com>
2023-12-25 23:30:16 +0300

    Update cmds.py

--- ZelzalMusic/plugins/play/cmds.py
+++ ZelzalMusic/plugins/play/cmds.py
@@ -20,7 +20,7 @@ async def zdatsr(client: Client, message: Message):
     name = usr.first_name
     usrnam = usr.username
     await message.reply_photo(
-        photo=f"https://graph.org/file/6270c9db2449eab390115.jpg",
+        photo=f"https://graph.org/file/1df1d4ee78c1dce31f4fe.jpg",
         caption=f"""<b>» مرحبـاً بك عـزيـزي </b> {message.from_user.mention} .\n\n<b>» استخـدم الازرار بالاسفـل 𝄞\n» لـ تصفـح اوامـر الميـوزك 🥁</b>""",
         reply_markup=InlineKeyboardMarkup(
             [
